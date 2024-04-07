8194d831ae677486a4938980aaf2cf76efae41c5
Yashwanth kumar R <97838432+yash52892@users.noreply.github.com>
2024-04-07 18:04:27 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,13 +2,19 @@ import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import AuthPage from "./Pages/Authpage";
 import Home from "./Pages/Home";
-
+import Compose from "./Pages/Compose";
+import Inbox from "./Pages/Inbox";
+import Sent from "./Pages/Sent";
 
 function App() {
   return (
     <Routes>
-    <Route path="/login" element={<AuthPage/>}/>
-    <Route path="/home" element={<Home/>}/>
+      <Route path="/login" element={<AuthPage />} />
+      <Route path="/home" element={<Home />} >
+      <Route path="inbox" element={<Inbox />} />
+      <Route path="sent" element={<Sent />} />
+      </Route>
+      <Route path="/compose" element={<Compose />} />
     </Routes>
   );
 }
